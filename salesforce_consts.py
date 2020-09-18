2e75cf84a68246a1ab107210085bb4489d78098c
cwadhwani <cwadhwani@splunk.com>
2020-09-18 23:46:57 +0530

    Salesforce: Quick change

--- salesforce_consts.py
+++ salesforce_consts.py
@@ -37,4 +37,4 @@ CASE_FIELD_MAP = {
 SALESFORCE_INVALID_INTEGER = 'Please provide non-zero positive integer in "{parameter}"'
 SALESFORCE_UNKNOWN_ERR_MSG = "Unknown error occurred. Please check the asset configuration and|or action parameters."
 SALESFORCE_ERR_CODE_UNAVAILABLE = "Error code unavailable"
-SALESFORCE_UNICODE_DAMMIT_TYPE_ERROR_MSG = "Error occurred while connecting to the Salesforce server. Please check the asset configuration and|or the action parameters."
+SALESFORCE_UNICODE_DAMMIT_TYPE_ERR_MSG = "Error occurred while connecting to the Salesforce server. Please check the asset configuration and|or the action parameters."
